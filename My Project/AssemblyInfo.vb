14be7b22afcb1d39957a1e51b516dbd335100ac2
MNicolso <71354582+MNicolso@users.noreply.github.com>
2020-10-08 16:56:05 -0700

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("TacocaT")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("TacocaT")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("085a9d42-a8ae-464e-a52d-54a229907dbf")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Scripts/script.js
@@ -0,0 +1,22 @@
+﻿const printBox = document.getElementById("printBox");
+const statusBox = document.getElementById("statusBox");
+
+document.getElementById("button").addEventListener('click', () => {
+    var inputBox = document.getElementById("inputBox").value;
+    var inputReverse = inputBox.split("").reverse().join("");
+    let input = inputBox.toLowerCase().replace(/\s|\'|\"/g, '');
+    let outputReverse = inputReverse.toLowerCase().replace(/\s|\'|\"/g, '');
+
+    printBox.innerHTML = outputReverse
+    verify(outputReverse, input)
+})
+    function verify(outputReverse, input) {
+        if (outputReverse === input) {
+            statusBox.innerHTML = "That's a palindrome!"
+        }
+        else if (outputReverse !== input) {
+            statusBox.innerHTML = "That's a palindon't!"
+        }
+    }
+
+
